1417652012cbc711c171adeeaebc4cbe1deefb8a
Nipun22 <nipunkataria98@gmail.com>
2019-02-01 21:44:03 -0800

    solution 1.4

--- chapter_1/1.4_palindrome_Permutation/palindromePermutation.js
+++ chapter_1/1.4_palindrome_Permutation/palindromePermutation.js
@@ -1,4 +1,22 @@
 const isPalindromePermutation = (str) => {
-    let trackLetters = new Object;
-    
+    str = str.toLowerCase(); // make the string case insensitive
+    arr = new Array('z'.charCodeAt(0) - 'a'.charCodeAt(0) + 1).fill(0);
+    for (let i = 0; i<str.length; i++) {
+        let c = str.charCodeAt(i) - 97;
+        if (c >= 0 && c <= 25) {
+            arr[c] += 1;
+        }
+    }
+    let odd = false
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 1) {
+            if (odd) {
+                return false
+            }
+            odd = true
+        }
+    }
+    return true;
 }
+
+console.log(isPalindromePermutation('tactcoa'));
